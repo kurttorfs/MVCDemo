5e6323c301dc855c32b77c8e978c58c4427c0b2a
Kurt Torfs <kurt.torfs@euri.com>
2016-04-07 11:59:26 +0200

    Added inspinia theme;

--- MVCDemo.Web/App_Start/BundleConfig.cs
+++ MVCDemo.Web/App_Start/BundleConfig.cs
@@ -8,11 +8,13 @@ namespace MVCDemo.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // Vendor scripts
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-2.2.2.min.js"));
 
+            // jQuery Validation
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            "~/Scripts/jquery.validate.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -23,12 +25,35 @@ namespace MVCDemo.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            // Inspinia script
+            bundles.Add(new ScriptBundle("~/bundles/inspinia").Include(
+                      "~/Scripts/app/inspinia.js"));
+
+            // SlimScroll
+            bundles.Add(new ScriptBundle("~/plugins/slimScroll").Include(
+                      "~/Scripts/plugins/slimScroll/jquery.slimscroll.min.js"));
+
+            // jQuery plugins
+            bundles.Add(new ScriptBundle("~/plugins/metsiMenu").Include(
+                      "~/Scripts/plugins/metisMenu/metisMenu.min.js"));
+
+            bundles.Add(new ScriptBundle("~/plugins/pace").Include(
+                      "~/Scripts/plugins/pace/pace.min.js"));
+
+            // Zendesk widget
             bundles.Add(new ScriptBundle("~/bundles/zendesk").Include(
                       "~/Scripts/ZendeskWidget.js"));
 
+            // CSS style (bootstrap/inspinia)
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/animate.css",
+                      //"~/Content/site.css",
+                      "~/Content/style.css"));
+
+            // Font Awesome icons
+            bundles.Add(new StyleBundle("~/font-awesome/css").Include(
+                      "~/fonts/font-awesome/css/font-awesome.min.css", new CssRewriteUrlTransform()));
         }
     }
 }
