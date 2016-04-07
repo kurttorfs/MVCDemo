a7b8ca63791ee902647c37e37f2e730946ef125c
ktorfs <kurt.torfs@euri.com>
2016-04-07 09:54:08 +0200

    MVCDEMO-1: Added zendesk widget to Layout;

--- MVCDemo.Web/App_Start/BundleConfig.cs
+++ MVCDemo.Web/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace MVCDemo.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/zendesk").Include(
+                      "~/Scripts/ZendeskWidget.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
